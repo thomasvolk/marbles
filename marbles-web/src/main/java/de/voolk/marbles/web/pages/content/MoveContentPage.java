962e2f3d4e37c48d2f771356bf93a1147a5f94ba
Thomas Volk <thomas@my-volk.de>
2011-07-21 19:29:08 +0200

    move (not finished)

--- marbles-web/src/main/java/de/voolk/marbles/web/pages/content/MoveContentPage.java
+++ marbles-web/src/main/java/de/voolk/marbles/web/pages/content/MoveContentPage.java
@@ -1,6 +1,8 @@
 package de.voolk.marbles.web.pages.content;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.WebComponent;
 
 import de.voolk.marbles.web.pages.content.panel.SiteMapPanel;
 import de.voolk.marbles.web.pages.content.sidebar.MoveContentSidebar;
@@ -8,6 +10,12 @@ import de.voolk.marbles.web.pages.content.sidebar.MoveContentSidebar;
 public class MoveContentPage extends AbstractContentPage {
 	private Component action;
 	private MoveContentSidebar moveContentSidebar;
+
+	public MoveContentPage(PageParameters parameters) {
+		super(parameters);
+		action = new WebComponent("action");
+        add(action);
+	}
 
 	@Override
 	protected void postInit() {
@@ -20,8 +28,12 @@ public class MoveContentPage extends AbstractContentPage {
 
 	@Override
 	protected Component createSidebarPanel(String id) {
+		Integer marblesPageId = getMarblesPageId();
+		if(marblesPageId == null) {
+			marblesPageId = getPageSession().getRootPage().getId();
+		}
 		moveContentSidebar = new MoveContentSidebar(id,
-				getMarblesPageId(), this);
+				marblesPageId, this);
 		return moveContentSidebar;
 	}
 
