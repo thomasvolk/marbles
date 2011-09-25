e6c0f1dcb4359ff1e2918f0bfb3aa2c7b807d35a
Thomas Volk <thomas@my-volk.de>
2011-09-25 10:15:23 +0200

    test create page

--- marbles-test/src/test/java/de/voolk/marbles/webtest/MarblesWebTest.java
+++ marbles-test/src/test/java/de/voolk/marbles/webtest/MarblesWebTest.java
@@ -1,6 +1,16 @@
 package de.voolk.marbles.webtest;
 
-import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkNotPresent;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
+import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
+import static net.sourceforge.jwebunit.junit.JWebUnit.clickElementByXPath;
+import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setTextField;
+import static net.sourceforge.jwebunit.junit.JWebUnit.submit;
+
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,13 +24,25 @@ public class MarblesWebTest {
 	@Test
 	public void testHomePage() {
 		beginAt("/");
-        assertLinkNotPresent("logoutLnk");
+        assertLinkNotPresent("logoutLink");
 		assertTitleEquals("Marbles");
 		setTextField("username", "test");
         setTextField("password", "test");
         submit();
-        assertLinkPresent("logoutLnk");
-        clickLink("logoutLnk");
+        
+        String subPageName = String.format("newSubpage_%s", new Date().getTime());
+        
+        clickLink("editPageLink");
+        setTextField("pageContent", "this test was inserted by the JWebUnitTest \n\n %%" + subPageName);
+        submit();
+        
+        clickElementByXPath("//*[contains(child::text(),'" + subPageName + "')]");
+        assertLinkPresent("cancelLink");
+        setTextField("pageContent", "this test was inserted by the JWebUnitTest for the subpage");
+        submit();
+        
+        assertLinkPresent("logoutLink");
+        clickLink("logoutLink");
 	}
 	
 }
