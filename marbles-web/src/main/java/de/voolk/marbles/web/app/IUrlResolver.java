793a3a8316ba261ad0a4bc4124e773384be2aab5
Thomas Volk XPS-L502X <thomas@my-volk.de>
2013-11-01 11:32:56 +0100

    license copyright

--- marbles-web/src/main/java/de/voolk/marbles/web/app/IUrlResolver.java
+++ marbles-web/src/main/java/de/voolk/marbles/web/app/IUrlResolver.java
@@ -2,13 +2,6 @@ package de.voolk.marbles.web.app;
 
 import de.voolk.marbles.api.beans.IPage;
 
-/**
- * Created by IntelliJ IDEA.
- * User: thomas
- * Date: 27.02.11
- * Time: 10:25
- * To change this template use File | Settings | File Templates.
- */
 public interface IUrlResolver {
     String getDisplayPageLink(int pageId);
 
